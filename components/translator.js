2fe7d1f7281a8ae65ea261c803da20cdf3bb44e7
SnowfallFox <icebornwhisker@gmail.com>
2024-11-25 13:01:41 +0000

    feat: error handling and initial planning

--- components/translator.js
+++ components/translator.js
@@ -5,6 +5,19 @@ const britishOnly = require('./british-only.js')
 
 class Translator {
 
+    // validate(puzzleString) {
+    //     let puzzleRegex = /[^0-9.]+/gmi
+    //     if (puzzleString && puzzleString.match(puzzleRegex)) {
+    //       return { error: 'Invalid characters in puzzle' }
+    //     } else if (String(puzzleString).length === 81) {
+    //       return true
+    //     } else {
+    //       return false
+    //     }
+    // }
+
+    
+
 }
 
 module.exports = Translator;
--- routes/api.js
+++ routes/api.js
@@ -8,6 +8,24 @@ module.exports = function (app) {
 
   app.route('/api/translate')
     .post((req, res) => {
-      
+      const text = req.body.text;
+      const locale = req.body.locale;
+      const validLocales = ['american-to-british', 'british-to-american'];
+
+      // if no text
+      if (!text) {
+        res.json({ error: 'No text to translate' })
+      // if locale is not one of the valid pre-defined options
+      } else if (!validLocales.includes(locale)) {
+        res.json({ error: 'Invalid value for locale field' })
+      } else {
+        // something like:
+        // Translator.translate(string, locale)
+        // (check for words in corresponding dict for translations, make array of words)
+        // for each word, replace in string with translation
+        // if changes made, return string
+        // if no changes made, return false
+      }
+
     });
 };
